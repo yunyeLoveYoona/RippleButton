613127eb59c7357e76e89a2870037d89054653d3
yunyeLoveYoona <550752356@qq.com>
2015-07-27 15:19:58 +0800

    给button添加错误时和正确时动画效果

---
+++ app/src/main/java/com/example/administrator/ripplebutton/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.administrator.ripplebutton;
+
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.administrator.ripplebutton.view.RippleButton;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+        final RippleButton rippleButton = (RippleButton) findViewById(R.id.rippleButton);
+        rippleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        rippleButton.showRight();
+                    }
+                },2000);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        rippleButton.showError();
+                    }
+                },4000);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
